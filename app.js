11661e4187d6880247f37e09fe028e7a017c1ea6
Pat P <patrickeperkins@gmail.com>
2023-06-01 11:46:26 -0400

    Lesson 9 commit

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
+require('dotenv').config()
+
 const express = require('express');
 const morgan = require('morgan')
+const mongoose = require('mongoose')
+const Blog = require('./models/blog')
+
+
 
 // express app
 const app = express();
 
+// connect to mongo
+const dbURI = process.env.ATLAS
+mongoose.connect(dbURI)
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err))
+
 // register view engine (ejs)
 app.set('view engine', 'ejs')
 
-// listen for requests
-app.listen(3000);
 
 // middleware & static files
 app.use(express.static('public'))
-
 app.use(morgan('dev'))
+
 
 
 app.get('/', (req, res) => {
     // res.send('<p>Home Page</p>');
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-      ];
-    res.render('index', { title: 'Home', blogs });
+    res.redirect('/blogs')
 });
 
 
@@ -31,6 +36,18 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About' })
 });
+
+
+// blog routes
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({createdAt: -1})
+        .then((result) => {
+            res.render('index', { title: 'All Blogs', blogs: result })
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
 
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create a New Blog' })
